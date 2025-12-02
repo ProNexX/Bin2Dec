e4c50de06b223acc15fbc42e4b1f163a7e225815
ProNexX <muhamed.syl321@gmail.com>
2025-12-02 17:58:08 +0100

    implemented bin 2 dec

--- Bin2Dec/Program.cs
+++ Bin2Dec/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿Console.WriteLine("Enter 'q' to quite the program");
+
+string? input = "";
+
+while (input != "q")
+{
+    Console.Write("Enter a binary number up to 8 digits: ");
+    input = Console.ReadLine();
+
+    if (input is null || input.Length > 8)
+    {
+        Console.WriteLine("Please enter a valid number!");
+        continue;
+    }
+
+    int sum = 0;
+    int expo = 0;
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        char num = input[i];
+        int numconverted = num - '0';
+
+        if (numconverted > 1 || numconverted < 0)
+        {
+            Console.WriteLine("Please enter a valid number!");
+            break;
+        }
+
+        sum += (int)Math.Pow(2, expo) * numconverted;
+        expo++;
+    }
+
+    Console.WriteLine($"Your decimal number is: {sum}");
+}
